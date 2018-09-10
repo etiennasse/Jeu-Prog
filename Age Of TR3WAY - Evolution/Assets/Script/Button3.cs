3d9da677f028d985819280ea6cacb55785de2d9c
B533P-02\533 <test@hotmail.com>
2018-09-10 17:18:55 -0400

    WayPoints

--- Age Of TR3WAY - Evolution/Assets/Script/Button3.cs
+++ Age Of TR3WAY - Evolution/Assets/Script/Button3.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Button3 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawn()
+    {
+
+        Quaternion rot = Quaternion.Euler(0, 90, 0);
+        Vector3 pos = new Vector3(15, 5, 205);
+        Instantiate(prefab, pos, rot);
+    }
 }
