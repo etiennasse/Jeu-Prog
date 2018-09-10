5db69e5f506b75088a1215e211ddadcc4b4fbc8a
B533P-02\533 <test@hotmail.com>
2018-09-10 14:39:17 -0400

    Map faite

---
+++ Age Of TR3WAY - Evolution/Assets/Script/CameraMove.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    [SerializeField] public float Movespeed = 10.0f;
+    [SerializeField] public float Zoomspeed = 0.5f;
+    [SerializeField] public float Rotation = 0.5f;
+    [SerializeField] public float MaxRotation = 0.3f;
+    [SerializeField] public float MinRotation = 0.1f;
+    [SerializeField] public float MaxDezoom = 40f;
+    [SerializeField] public float MaxZoom = 6f;
+
+    int boundary = 30;
+    int width;
+    int height;
+
+    void Start()
+    {
+        width = Screen.width;
+        height = Screen.height;
+    }
+
+    void Update()
+    {
+
+            if (Input.mousePosition.x > width - boundary && transform.position.x < 170)
+            {
+                transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * -Movespeed, 0.0f, 0.0f);
+                print(transform.position.x);
+            }
+
+            if (Input.mousePosition.x < 0 + boundary && transform.position.x > 25)
+            {
+                transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * -Movespeed, 0.0f, 0.0f);
+                print(transform.position.x);
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0) //Zoom
+            {
+                if (transform.position.y >= MaxZoom)
+                {
+                    GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y - Zoomspeed, transform.position.z + Zoomspeed);
+                }
+                if (transform.rotation.x > MinRotation && transform.position.y <= 30)
+                {
+                    transform.Rotate(-Rotation, 0, 0);
+                }
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0) //Dezoom
+            {
+            if (transform.position.y <= MaxDezoom)
+            {
+                GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y + Zoomspeed, transform.position.z - Zoomspeed);
+            }
+            if (transform.rotation.x < MaxRotation )
+                {
+                    transform.Rotate(Rotation, 0, 0);
+                }
+            }
+    }
+}
