1e1e881f795eec02be761ddf5fe0540957ef9018
B533P-02\533 <test@hotmail.com>
2018-11-09 13:40:32 -0500

    Réajout du soundtrack

---
+++ Age Of TR3WAY - Evolution/Assets/Script/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake() {
+        int numOfMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+        if(numOfMusicPlayer > 1) {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+}
